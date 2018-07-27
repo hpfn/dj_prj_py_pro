0a182b1579f139d1c2b577a1182c226456091a3d
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-07-27 16:44:58 -0300

    Build moveis/models.py close #45

--- certgen/moveis/models.py
+++ certgen/moveis/models.py
@@ -1,3 +1,14 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Movel(models.Model):
+    titulo = models.CharField(max_length=50)
+    preco = models.DecimalField(decimal_places=2, max_digits=10)
+    descricao = models.TextField()
+    foto = models.ImageField(upload_to='moveis/', default='http://foo')
+
+    def __str__(self):
+        return self.titulo
+
+    def __repr__(self):
+        return f'Movel(titulo={self.titulo!r}, preco={self.preco!r}, descricao={self.descricao!r})'
